a289e7fac19bac7f877ea6934bcf604b61d60fae
Krystian Żybura <Krystian12324444@gmail.com>
2023-12-02 17:19:35 +0100

    refactored

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 "use strict";
 {
-    const toggleTheme = () => {
-        const body = document.querySelector(".js-body");
-        const header = document.querySelector(".js-header");
-        const footer = document.querySelector(".js-footer");
-        if (body && header && footer) {
-            body.classList.toggle("body--dark");
-            header.classList.toggle("header--dark");
-            footer.classList.toggle("footer--dark");
-        }
-    };
-    const toggleThemeName = () => {
-        const themeName = document.querySelector(".js-themeName");
-        if (themeName) {
-            themeName.innerText =
-                themeName.innerText === "Ciemny" ? "Jasny" : "Ciemny";
-        }
-    };
-    const onChangeThemeButton = () => {
-        toggleTheme();
-        toggleThemeName();
-    };
-    const init = () => {
-        const changeThemeButton = document.querySelector(".js-button");
-        if (changeThemeButton) {
-            changeThemeButton.addEventListener("click", onChangeThemeButton);
-        }
-    };
-    init();
+  const toggleTheme = () => {
+    const body = document.querySelector(".js-body");
+    const header = document.querySelector(".js-header");
+    const footer = document.querySelector(".js-footer");
+    if (body && header && footer) {
+      body.classList.toggle("body--dark");
+      header.classList.toggle("header--dark");
+      footer.classList.toggle("footer--dark");
+    }
+  };
+
+  const toggleThemeName = () => {
+    const themeName = document.querySelector(".js-themeName");
+    if (themeName) {
+      themeName.innerText =
+        themeName.innerText === "Ciemny" ? "Jasny" : "Ciemny";
+    }
+  };
+
+  const onChangeThemeButton = () => {
+    toggleTheme();
+    toggleThemeName();
+  };
+
+  const init = () => {
+    const changeThemeButton = document.querySelector(".js-button");
+    if (changeThemeButton) {
+      changeThemeButton.addEventListener("click", onChangeThemeButton);
+    }
+  };
+
+  init();
 }
